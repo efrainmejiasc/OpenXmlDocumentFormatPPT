d165ae511ad19cec100fc35dab62e00c08686f80
efrainmejiasc <efrainmejiasc@hotmail.com>
2020-10-02 12:18:16 -0400

    Update

--- OpenXmlDocumentFormatPPT/OpenXmlDocumentFormatPPT/OpenDocumentXmlFormatPPT.cs
+++ OpenXmlDocumentFormatPPT/OpenXmlDocumentFormatPPT/OpenDocumentXmlFormatPPT.cs
@@ -1,4 +1,5 @@
-﻿using DocumentFormat.OpenXml.Packaging;
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Presentation;
 using System;
 using System.Collections.Generic;
@@ -6,54 +7,33 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Web;
+using System.Xml;
 
 namespace OpenXmlDocumentFormatPPT
 {
     public class OpenDocumentXmlFormatPPT
     {
+
         public bool WriteOnSlide()
         {
             string filePath = System.Web.HttpContext.Current.Server.MapPath("~/TemplatePPT/Plantilla1.pptx");
             try
             {
-                using (PresentationDocument presentationDocument = PresentationDocument.Open(filePath, true))
+                using (PresentationDocument documentoPP = PresentationDocument.Open(filePath, true))
                 {
-                    PresentationPart presentationPart = presentationDocument.PresentationPart;
-                    Presentation presentation = presentationPart.Presentation;
-                    List<Shape> shapes = new List<Shape>();
-                    foreach (var slideId in presentation.SlideIdList.Elements<SlideId>())
-                    {
-                        SlidePart slidePart = presentationPart.GetPartById(slideId.RelationshipId) as SlidePart;
-                        shapes = (from shape in slidePart.Slide.Descendants<Shape>()
-                                     where IsTitleShape(shape)
-                                     select shape).ToList();
-                   
-                    }
-                    StringBuilder paragraphText = new StringBuilder();
-                    var shape0 = shapes[0];
-
-                    string paragraphSeparator = null;
-                    foreach (var shape in shapes)
-                    {
-                        // Get the text in each paragraph in this shape.
-                        foreach (var paragraph in shape.TextBody.Descendants())
-                        {
-                            var a = paragraph;
-                            // Add a line break.
-                            paragraphText.Append(paragraphSeparator);
-
-                            foreach (var text in paragraph.Descendants())
-                            {
-                                var b = text;
-                                paragraphText.Append(text);
-                            }
-
-                            paragraphSeparator = "\n";
-                        }
-                    }
-                    int n = 0;
-
+                    PresentationPart partesDocumento = documentoPP.PresentationPart;
+                    Presentation presentacion = partesDocumento.Presentation;
+                    OpenXmlElementList diapositivas = partesDocumento.Presentation.SlideIdList.ChildElements;
+                    string idDiapositiva = (diapositivas[0] as SlideId).RelationshipId;
+                    SlidePart diapositiva = (SlidePart) partesDocumento.GetPartById(idDiapositiva);
+                    List<OpenXmlElement> itemsdiapositiva = diapositiva.Slide.CommonSlideData.ShapeTree.ChildElements.ToList();
+                    string titulo = itemsdiapositiva[2].InnerText;
+                    string subTitulo = itemsdiapositiva[3].InnerText;
+                    itemsdiapositiva[2].InnerXml = itemsdiapositiva[2].InnerXml.Replace(titulo, "Este es el titulo de la diapositiva: " + DateTime.Now.ToString());
+                    itemsdiapositiva[3].InnerXml = itemsdiapositiva[3].InnerXml.Replace(subTitulo, "Este es el subtitulo de la diapositiva: " + DateTime.Now.ToString());
+                    partesDocumento.Presentation.Save();
                 }
+
                 ReadWriteTxt(filePath);
             }
             catch (Exception ex)
@@ -64,27 +44,6 @@ namespace OpenXmlDocumentFormatPPT
             return true;
         }
 
-        private static bool IsTitleShape(Shape shape)
-        {
-            // t.InnerText = "Escribiendo en Titulo: " + DateTime.Now.ToString();
-            var placeholderShape = shape.NonVisualShapeProperties.ApplicationNonVisualDrawingProperties.GetFirstChild<PlaceholderShape>();
-            if (placeholderShape != null && placeholderShape.Type != null && placeholderShape.Type.HasValue)
-            {
-                switch ((PlaceholderValues)placeholderShape.Type)
-                {
-                    // Any title shape.
-                    case PlaceholderValues.Title:
-
-                    // A centered title.
-                    case PlaceholderValues.CenteredTitle:
-                        return true;
-
-                    default:
-                        return false;
-                }
-            }
-            return false;
-        }
 
         private void ReadWriteTxt(string pathArchivo)
         {
