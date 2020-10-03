36706d42c417bfe153fca105829f93e610ba976f
efrainmejiasc <efrainmejiasc@hotmail.com>
2020-10-03 12:44:08 -0400

    update

--- OpenXmlDocumentFormatPPT/OpenXmlDocumentFormatPPT/OpenDocumentXmlFormatPPT.cs
+++ OpenXmlDocumentFormatPPT/OpenXmlDocumentFormatPPT/OpenDocumentXmlFormatPPT.cs
@@ -1,13 +1,16 @@
 ﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Drawing;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Presentation;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Web;
 using System.Xml;
+
 
 namespace OpenXmlDocumentFormatPPT
 {
@@ -17,6 +20,9 @@ namespace OpenXmlDocumentFormatPPT
         public bool WriteOnSlide()
         {
             string filePath = System.Web.HttpContext.Current.Server.MapPath("~/TemplatePPT/Plantilla1.pptx");
+            string tituloText = "Este es el titulo de la diapositiva: " + DateTime.Now.ToString();
+            string subTituloText = "Este es el subtitulo de la diapositiva: " + DateTime.Now.ToString();
+
             try
             {
                 using (PresentationDocument documentoPP = PresentationDocument.Open(filePath, true))
@@ -27,10 +33,12 @@ namespace OpenXmlDocumentFormatPPT
                     string idDiapositiva = (diapositivas[0] as SlideId).RelationshipId;
                     SlidePart diapositiva = (SlidePart) partesDocumento.GetPartById(idDiapositiva);
                     List<OpenXmlElement> itemsdiapositiva = diapositiva.Slide.CommonSlideData.ShapeTree.ChildElements.ToList();
-                    string titulo = itemsdiapositiva[2].InnerText;
-                    string subTitulo = itemsdiapositiva[3].InnerText;
-                    itemsdiapositiva[2].InnerXml = itemsdiapositiva[2].InnerXml.Replace(titulo, "Este es el titulo de la diapositiva: " + DateTime.Now.ToString());
-                    itemsdiapositiva[3].InnerXml = itemsdiapositiva[3].InnerXml.Replace(subTitulo, "Este es el subtitulo de la diapositiva: " + DateTime.Now.ToString());
+                    DocumentFormat.OpenXml.Presentation.Shape titulo = (DocumentFormat.OpenXml.Presentation.Shape)itemsdiapositiva[2];
+                    DocumentFormat.OpenXml.Presentation.Shape subTitulo = (DocumentFormat.OpenXml.Presentation.Shape)itemsdiapositiva[3];
+                    titulo.InnerXml = GetXmlStringBody(titulo.InnerXml, tituloText);
+                    subTitulo.InnerXml = GetXmlStringBody(subTitulo.InnerXml, subTituloText);
+
+
                     partesDocumento.Presentation.Save();
                 }
 
@@ -42,6 +50,72 @@ namespace OpenXmlDocumentFormatPPT
             }
 
             return true;
+        }
+
+
+
+
+        private string  GetXmlStringBody(string innerXml,string texto)
+        {
+            if (innerXml.Contains("<a:t>"))
+             innerXml = System.Text.RegularExpressions.Regex.Replace(innerXml, @"<a:t(.*?)>(.*?)</a:t>", "<a:t>" + texto + "</a:t>");
+
+             return innerXml;
+        }
+
+
+        public Paragraph ReplaceText(Paragraph paragraph)
+        {
+
+          var parent = paragraph.Parent; //get parent element - to be used when removing placeholder
+            var dataParam = new PowerPointParameter()
+            {
+                Name = "Titulo Set",
+                Text = "Texto para escribir"
+            };
+
+      
+                //insert text list
+                if (dataParam.Name.Contains("string[]")) //check if param is a list
+                {
+                    var arrayText = dataParam.Text.Split(Environment.NewLine.ToCharArray()); //in our case we split it into lines
+
+                    if (arrayText is IEnumerable) //enumerate if we can
+                    {
+                        foreach (var itemData in arrayText)
+                        {
+                            Paragraph bullet = CloneParaGraphWithStyles(paragraph, dataParam.Name, itemData);// create new param - preserve styles
+                            parent.InsertBefore(bullet, paragraph); //insert new element
+                        }
+                    }
+                    paragraph.Remove();//delete placeholder
+                }
+                else
+                {
+                    //insert text line
+                    var param = CloneParaGraphWithStyles(paragraph, dataParam.Name, dataParam.Text); // create new param - preserve styles
+                    parent.InsertBefore(param, paragraph);//insert new element
+
+                    paragraph.Remove();//delete placeholder
+                }
+
+            return paragraph;
+        }
+
+        public class PowerPointParameter
+        {
+            public string Name { get; set; }
+            public string Text { get; set; }
+            public FileInfo Image { get; set; }
+        }
+
+        public static Paragraph CloneParaGraphWithStyles(Paragraph sourceParagraph, string paramKey, string text)
+        {
+            var xmlSource = sourceParagraph.OuterXml;
+
+            xmlSource = xmlSource.Replace(paramKey.Trim(), text.Trim());
+
+            return new Paragraph(xmlSource);
         }
 
 
